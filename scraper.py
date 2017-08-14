8e3514afc35ac979fa51dc5b2e17baf352a60b8f
nick-reynolds <reynolds.nicholas@gmail.com>
2017-08-14 10:11:02 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,14 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from splinter import Browser
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+with Browser("phantomjs") as browser:
+    # Optional, but make sure large enough that responsive pages don't
+    # hide elements on you...
+    browser.driver.set_window_size(1280, 1024)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    # Open the page you want...
+    browser.visit("https://www.racingpost.com/profile/owner/127223/ballymacoll-stud/entries")
+
+    # Scrape the data you like...
+    entries = browser.find_by_css(".pp-entries__item")
+    for entry in entries:
+        print entry['div.pp-entries__item__header']
